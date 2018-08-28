d89dc55fc2e83f82a32839e6c490266be6269398
Aaron Johnson <aaron@aarons-linux-mint-18.3-sylvia>
2018-08-27 19:11:48 -0500

    Adds the basic program structure to hw1-aaronjohnson.cpp

--- hw1/hw1-aaronjohnson.cpp
+++ hw1/hw1-aaronjohnson.cpp
@@ -1,2 +1,24 @@
-// Aaron Johnson - aaron.r.johnson@und.edu
-// CSci451       - Operating Systems
+//Author:     Aaron Johnson
+//Email:      aaron.r.johnson@und.edu
+//Course:     CSci451
+//Instructor: Dr. Ronald Marsh
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char *argv []) 
+{
+
+    if (argc > 5) {
+        printf("Invalid arguments.\nUsage: ./hw1-aaronjohnson <UNIXcommands>\nArgs: <UNIXcommands> - any UNIX commands you'd like to execute. You may use up to 4 command line args.\n");
+        exit(0);
+    }
+
+    system("ls -ls >> output.txt");
+
+    cout << "Hello, World!\n";
+    return 0;
+}
