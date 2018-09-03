e41c3528827e1573e5fc4dc6325671198bed0872
arj0819 <aaronandsydney0819@gmail.com>
2018-09-03 12:21:57 -0500

    Adds hw2

---
+++ hw2/hw2-aaronjohnson.c
@@ -0,0 +1,19 @@
+//Author:     Aaron Johnson
+//Email:      aaron.r.johnson@und.edu
+//Course:     CSci451
+//Instructor: Dr. Ronald Marsh
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv []) 
+{
+    for (int i = 1; i < argc; i++) {
+        char unixCommand[1000];
+        strcpy(unixCommand, argv[i]);
+        strcat(unixCommand, " >> output.txt");
+        system(unixCommand);
+    }
+    return 0;
+}
