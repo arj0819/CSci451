690873e7b5958a8d5d84b1b01bf816305a96f8f1
arj0819 <aaronandsydney0819@gmail.com>
2018-09-29 23:06:29 -0500

    Build: May complete the assignment, further testing required

--- hw5/hw5-aaronjohnson.cpp
+++ hw5/hw5-aaronjohnson.cpp
@@ -50,7 +50,7 @@ const int RAND_SEED = 1492;
 struct MemoryPage {
     int modPID = 0;
     int R_bit = 0;
-    int W_bit = 0;
+    int M_bit = 0;
     int isClaimed = 0;
 };
 MemoryPage memoryPages[NUM_PAGES]; 
@@ -65,12 +65,12 @@ int main(int argc, char *argv[])
         thread m1(manipulator1);
         thread m2(manipulator2);
         thread m3(manipulator3);
-        // thread gc(garbageCollector);
+        thread gc(garbageCollector);
 
         m1.join();
         m2.join();
         m3.join();
-        // gc.join();
+        gc.join();
     }
 
     return 0;
@@ -98,26 +98,53 @@ void manipulator3() {
 }
 
 void garbageCollector() {
-    
+    for (int i = 0; i < NUM_PAGES; i++) {
+        if (memoryPages[i].isClaimed) {
+            // printf("Garbage Collector is attempting to access Page %d\n\n",i);
+            lock_guard <mutex> guard(pageMutex);
+            // puts("GARBAGE");
+            // printf("Page: %d\nR bit: %d\nM bit: %d\nPID: %d\n",i,memoryPages[i].R_bit,memoryPages[i].M_bit,memoryPages[i].modPID);
+            memoryPages[i].isClaimed = 0;
+            memoryPages[i].modPID = 0;
+            if (memoryPages[i].M_bit == 1) {
+                memoryPages[i].R_bit = 0;
+                memoryPages[i].M_bit = 0;
+                // puts("SLEEP (M_Bit = 1)");
+                this_thread::sleep_for(chrono::milliseconds(0));
+            // printf("Page: %d\nR bit: %d\nM bit: %d\nPID: %d\n",i,memoryPages[i].R_bit,memoryPages[i].M_bit,memoryPages[i].modPID);
+            } else {
+                // puts("NO SLEEP (M_Bit = 0)");
+                memoryPages[i].R_bit = 0;
+            }
+            // printf("Garbage Collector is releasing Page %d\n\n",i);
+        } else {
+
+            // printf("No garbage found on Page %d\n\n",i);
+        }
+    }
 }
 
 void accessMemoryPage(int pageIndex, int randAccess, int pid) {
     if (!memoryPages[pageIndex].isClaimed) {
+        // printf("Accessor %d is attempting to access Page %d\n\n",pid,pageIndex);
         lock_guard <mutex> guard(pageMutex);
         memoryPages[pageIndex].isClaimed = 1;
         int r_bit = 0;
-        int w_bit = 0;
-        if (randAccess) {
+        int m_bit = 0;
+        if (randAccess == 1) {
             r_bit = 1;
-            w_bit = 1;
+            m_bit = 1;
         } else {
             r_bit = 1;
         }
         memoryPages[pageIndex].R_bit = r_bit;
-        memoryPages[pageIndex].W_bit = w_bit;
+        memoryPages[pageIndex].M_bit = m_bit;
         memoryPages[pageIndex].modPID = pid;
-        printf("Page Accessed: %d\nR bit: %d\nW bit: %d\nPID: %d\n\n",pageIndex,r_bit,w_bit,pid);
+        // puts("ACCESS");
+        // printf("Page: %d\nR bit: %d\nM bit: %d\nPID: %d\n\n",pageIndex,r_bit,m_bit,pid);
+        // printf("Accessor %d is releasing Page %d\n\n",pid,pageIndex);
     } else {
-        printf("Page Fault: %d\n", pageIndex);
+        printf("\nPage Fault: %d\n", pageIndex);
+        // printf("Accessor %d was denied access to Page %d\n\n",pid,pageIndex);
     }
 }
