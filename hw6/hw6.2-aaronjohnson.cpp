7b82c22319e0ae4bc1337d8e07496aaf085fbef7
arj0819 <aaronandsydney0819@gmail.com>
2018-10-01 23:35:25 -0500

    Build: Adds hw6 and starting template

---
+++ hw6/hw6.2-aaronjohnson.cpp
@@ -0,0 +1,26 @@
+//Author:     Aaron Johnson
+//Email:      aaron.r.johnson@und.edu
+//Course:     CSci451
+//Instructor: Dr. Ronald Marsh
+//Assignment: hw6
+
+/*
+    This is program 3 of 4 for this assignment. This program
+    will always be executed first and will create a set of
+    semaphores for the rest of the programs to use.
+*/
+
+#include <cstdio>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+int semaphore1 = 0;
+int semaphore2 = 1;
+
+int main(int argc, char *argv[]) {
+    puts("Executing process #2");
+
+    return 0;
+}
