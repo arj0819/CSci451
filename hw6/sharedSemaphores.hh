087cf7abac91161aa88506c27f58bbfa09629759
arj0819 <aaronandsydney0819@gmail.com>
2018-10-02 21:42:20 -0500

    Build: Adds another semaphore in case we need it

--- hw6/sharedSemaphores.hh
+++ hw6/sharedSemaphores.hh
@@ -1,5 +1,6 @@
 #include <semaphore.h>
-#ifndef SEMAPHORE_1 SEMAPHORE_2
-#define SEMAPHORE_1 "sem1"
-#define SEMAPHORE_2 "sem2"
+#ifndef SEM_1 SEM_2 SEM_3
+#define SEM_1 "sem1"
+#define SEM_2 "sem2"
+#define SEM_3 "sem3"
 #endif
