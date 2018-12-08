3dd4ddb51c53a0c3d4b4ed09bf8ac379b8f18def
arj0819 <aaronandsydney0819@gamil.com>
2018-12-07 21:24:34 -0600

    Build: Adds code that may be used on marsh's 451 final

---
+++ MarshFinal/marsh.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+using namespace std;
+
+unsigned char CLAMP(int input)
+{
+    if(input < 0) return 0;
+    if(input > 255) return 255;
+}
+
+int ROUND(float input)
+{
+    return (int)floor(input + .5);
+}
+
+int main(void)
+{
+    int temp;
+    float i = 3.14159;
+    temp = ROUND(i);
+    printf("%d\n", CLAMP(temp));
+    printf("%d\n", CLAMP(ROUND(i)));
+    return 0;
+}
